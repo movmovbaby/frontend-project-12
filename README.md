644746f34e69db0fd1d1322f1fc0c6256cbe7866
Nikolai Ochirov <dubdubman@ya.ru>
2022-12-03 00:39:00 +0300

    step 11, add rollbar, fix readme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 [![Actions Status](https://github.com/movmovbaby/frontend-project-12/workflows/hexlet-check/badge.svg)](https://github.com/movmovbaby/frontend-project-12/actions)
 
 ### App
-[Link](https://frontend-project-12-production-0628.up.railway.app) to app
+[Link](https://frontend-project-12-production-3748.up.railway.app) to app
--- frontend/src/init.js
+++ frontend/src/init.js
@@ -2,6 +2,7 @@ import { I18nextProvider, initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
 import resources from './locales/index.js';
 import { Provider as StoreProvider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import store from './slices/index.js';
 import App from './components/App.jsx';
 import { io } from 'socket.io-client';
@@ -9,6 +10,13 @@ import { io } from 'socket.io-client';
 const init = async () => {
   const socket = io();
   const i18nInstance = i18n.createInstance();
+
+  const rollbarConfig = {
+    accessToken: '74bc07850c6f47229b2c36a0b6b1a62f',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  }
+
 
   await i18nInstance
     .use(initReactI18next)
@@ -18,11 +26,15 @@ const init = async () => {
     });
 
   return (
-    <I18nextProvider i18n={i18nInstance}>
-      <StoreProvider store={store}>
-        <App socket={socket} />
-      </StoreProvider>
-    </I18nextProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <I18nextProvider i18n={i18nInstance}>
+          <StoreProvider store={store}>
+            <App socket={socket} />
+          </StoreProvider>
+        </I18nextProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   )
 };
 
